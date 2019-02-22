b38d501990d50a6e2ae616df0a53804757070926
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2019-02-23 00:50:30 +0200

    Implement movies tableview datasource and show titles in table cellse

--- MoviesDB/MoviesController.swift
+++ MoviesDB/MoviesController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class MoviesController: UITableViewController {
-
+    var dataSource = MoviesTableViewDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "All Movies"
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = dataSource
+        dataSource.dataChanged = { [weak self] in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 }
 
--- MoviesDB/MoviesTableViewDataSource.swift
+++ MoviesDB/MoviesTableViewDataSource.swift
@@ -6,4 +6,41 @@
 //  Copyright © 2019 Mahmoud Abolfotoh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MoviesTableViewDataSource: NSObject, UITableViewDataSource {
+    var page = 1
+    var movies = [Movie]()
+    var dataChanged: (() -> Void)?
+    let cellId = "Cell"
+
+    init(isTesting: Bool = false) {
+        super.init()
+        if !isTesting {
+            getMovies(at: page)
+        }
+    }
+    
+    func getMovies(at page: Int) {
+        Movie.fetch(page: page, onSuccess: { (response) in
+            self.movies = response.movies
+            self.dataChanged?()
+        })
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let movie = movies[indexPath.row]
+        cell.textLabel?.text = movie.title
+        return cell
+    }
+}
+

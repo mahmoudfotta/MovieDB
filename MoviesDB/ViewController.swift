23abde9a626f6613d8b0f7c9ddf2b89a6f207acc
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2019-02-22 17:20:20 +0200

    Initial Commit

---
+++ MoviesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesDB
+//
+//  Created by Mahmoud Abolfotoh on 2/22/19.
+//  Copyright © 2019 Mahmoud Abolfotoh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

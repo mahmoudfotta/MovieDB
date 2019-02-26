e008e15e793094d959760762736075b598e229ea
Mahmoud <mahmoud_gamal_abolfotoh@hotmail.com>
2019-02-26 20:56:18 +0200

    Rearrange files into groups

---
+++ MoviesDB/Delegates/ImagePickerDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  ImagePickerDelegate.swift
+//  MoviesDB
+//
+//  Created by Mahmoud Abolfotoh on 2/26/19.
+//  Copyright © 2019 Mahmoud Abolfotoh. All rights reserved.
+//
+
+import Foundation
